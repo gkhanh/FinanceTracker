47fd178f605591cdbb85b7d911c31c538beec206
gkhanh <493341@student.saxion.nl>
2023-01-24 14:58:01 +0100

    fix Azure cannot display the website

---
+++ Models/UserLoginInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finance_Tracking_Web_Application.Models
+{
+    public class UserLoginInfo
+    {
+        public string LoginId { get; set; }
+        public string Password { get; set; }
+    }
+}
