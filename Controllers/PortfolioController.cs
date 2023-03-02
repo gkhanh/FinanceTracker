f757940e114a1532e904fa27d09db270b24f6cc7
Khanh Le <493341@student.saxion.nl>
2023-03-02 11:16:42 +0100

    Update code for Portfolio

---
+++ Controllers/PortfolioController.cs
@@ -0,0 +1,21 @@
+﻿using Finance_Tracking_Web_Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Finance_Tracking_Web_Application.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        private readonly ILogger<PortfolioController> _logger;
+
+        public PortfolioController(ILogger<PortfolioController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
